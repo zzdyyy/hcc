f195ec2a9860f99a511557b62cd619bd4cc1b90b
zzdyyy <zzdyyy@qq.com>
2016-12-01 16:13:42 +0800

    Primary code generation finished
    finished the x86 assemble code generation, but there are some bugs:
    1. not use a BYTE PTR, which may be affected char and char[] assignment
    2. int[] seems makes a dead loop
    and there are some new syntax bug found:
    1. some code may cause hcc to abort

--- genasm.cpp
+++ genasm.cpp
@@ -3,18 +3,15 @@
 static ostringstream out;
 static funcitem *fitem = nullptr;
 
-//some different name for different type
-string ptr_arr[] = {"DWORD PTR", "BYTE PTR"};
-string mov_arr[] = {"mov  ", "movsx"};
-string eax_arr[] = {"eax", "al"};
-inline int t2i(int itype_t) { return itype_t-1; }//type_t to index
-inline string &ptr(qoperand q) { return ptr_arr[t2i(getitem(q).datatype)]; }
-inline string &mov(qoperand q) { return mov_arr[t2i(getitem(q).datatype)]; }
-inline string &eax(qoperand q) { return eax_arr[t2i(getitem(q).datatype)]; }
-
 void genheader()
 {
-    //TODO: out << ".386";
+    out << ".386" << endl
+        << ".model flat, stdcall" << endl
+        << "option casemap:none" << endl
+        << "include c:/masm32/include/msvcrt.inc" << endl
+        << "includelib c:/masm32/lib/msvcrt.lib" << endl
+        << "" << endl
+        ;
 }
 
 void gendataseg()
@@ -44,16 +41,21 @@ void gendataseg()
     //all string value
     for(unsigned int i=0; i<strtbl.size(); ++i)
     {
-        out << "    s" << i << " db \"" << strtbl[i] << "\",0" << endl;
+        if(strtbl[i] == "")
+            out << "    s" << i << " db 0" << endl;
+        else
+            out << "    s" << i << " db \"" << strtbl[i] << "\",0" << endl;
     }
     //format control string
     out << "    fmtd db \"%d\",0" << endl;
     out << "    fmtc db \"%c\",0" << endl;
     out << "    fmts db \"%s\",0" << endl;
     out << "    fmtnl db 10,0" << endl;//newline
+    out << endl;
 }
 
 //return the addressing string of immediate number / temp number / simple var/const
+//or base address of array
 //immediate number      10 -> 10
 //temp number           $5 -> DWORD PTR [ebp-20]
 //simple var/const      glb -> BYTE PTR _glb
@@ -70,11 +72,10 @@ string addrstr(qoperand qo)
                 + "]";//the first local var is [ebp-4]
 
     tblitem &ti = getitem(qo);
-    assert(!ti.isarray);
 
     if(qo.type == qoperand::GLB_OBJ)//global object
     {
-        return ptr(qo)+"_"+ti.name;
+        return "DWORD PTR _"+ti.name;
     }
 
     //local object
@@ -83,17 +84,32 @@ string addrstr(qoperand qo)
                 tostr(8 + qo.value*4)//the first arg is [ebp+8]
                 + "]";
     else
-        return ptr(qo)
-                +string(" [ebp-")
+        return "DWORD PTR [ebp-"
                 +tostr(4+ti.addr)//the first local var is [ebp-4]
                 +"]";
+}
+
+string cmpopstr(int op)
+{
+    switch(op)
+    {
+        case qi::JEQ: return("jz ");
+        case qi::JNE: return("jne");
+        case qi::JGE: return("jge");
+        case qi::JGT: return("jg ");
+        case qi::JLE: return("jle");
+        case qi::JLT: return("jl ");
+    }
+    return ("ERR");
 }
 
 void gencodeseg()
 {
     out << ".code" << endl;
     out << "init:" << endl;
-    out << "..." << endl << endl;//TODO
+    out << "    call  _main" << endl;
+    out << "    invoke crt_exit" << endl;
+    out << endl ;
 
     context = 0;
     for(funcitem &fi: functbl)
@@ -105,6 +121,16 @@ void gencodeseg()
             << "    push  ebp" << endl
             << "    mov   ebp, esp" << endl
             << "    sub   esp, " << fi.varsize+fi.tmpamt*4 << endl;
+        //set value for constant first
+        int i = 0;
+        for(tblitem ti: functbl[context].lcltbl)
+        {
+            if(ti.objtype == tblitem::CONST)
+                out << "    mov   "<<addrstr(qoperand{qoperand::LCL_OBJ, i})
+                    << ", " << ti.value <<endl;
+            ++i;
+        }
+        //generate asm for statements
         for(qi &q: fi.qilist)
         {
             switch(q.op)
@@ -115,21 +141,126 @@ void gencodeseg()
 
             case qi::RET:
                 if(q.A.type != qoperand::BLANK)
-                    out << "    " << mov(q.A) << " " << eax(q.A) << ", " << addrstr(q.A) << endl;
+                    out << "    mov   eax, " << addrstr(q.A) << endl;
                 out << "    jmp   " << lblexit << endl;
                 break;
 
             case qi::WR:
-                if(qi.A.type == qoperand::STRING)
-                    out<<"    invoke crt_printf,addr fmts,addr s"<<qi.A.value<<endl;
+                if(q.A.type == qoperand::STRING)
+                    out<<"    invoke crt_printf,addr fmts,addr s"<<q.A.value<<endl;
+                else
+                {
+                    out << "    mov   eax, " << addrstr(q.A) << endl;
+                    if(q.A.type == qoperand::IMDCHAR ||
+                       ((q.A.type == qoperand::LCL_OBJ || q.A.type == qoperand::GLB_OBJ) && getitem(q.A).datatype == char_t ))
+                        out<<"    invoke crt_printf,addr fmtc,eax"<<endl;
+                    else
+                        out<<"    invoke crt_printf,addr fmtd,eax"<<endl;
+                }
+                break;
 
-            case
+            case qi::RD:
+                out << "    lea   eax, " << addrstr(q.A) << endl;
+                if( getitem(q.A).datatype == char_t )
+                    out<<"    invoke crt_scanf,addr fmtc,eax"<<endl;
+                else
+                    out<<"    invoke crt_scanf,addr fmtd,eax"<<endl;
+                break;
+
+            case qi::PUSH:
+                out<<"    mov   eax, "<<addrstr(q.A)<<endl;
+                out<<"    push  eax"<<endl;
+                break;
+
+            case qi::CALL:
+                out<<"    call  _"<<glbtbl[q.A.value].name<<endl;
+                out<<"    add   esp, "<<functbl[glbtbl[q.A.value].value].argnum*4<<endl;
+                out<<"    mov   "<<addrstr(q.D)<<", eax"<<endl;
+                break;
+
+            case qi::ARRAYLOAD:
+                out<<"    lea   edx, "<<addrstr(q.A)<<endl;//base addr
+                out<<"    mov   eax, "<<addrstr(q.B)<<endl;//index
+                out<<"    mov   eax, DWORD PTR [edx+eax*"<<
+                    size_of[getitem(q.A).datatype]<<"]"<<endl;//load
+                out<<"    mov   "<<addrstr(q.D)<<", eax"<<endl;//store
+                break;
+
+            case qi::ARRAYASS:
+                out<<"    lea   edx, "<<addrstr(q.A)<<endl;//base addr
+                out<<"    mov   eax, "<<addrstr(q.B)<<endl;//index
+                out<<"    mov   ecx, "<<addrstr(q.D)<<endl;//value
+                out<<"    mov   DWORD PTR [edx+eax*"<<
+                    size_of[getitem(q.A).datatype]<<"], ecx"<<endl;//assign
+                break;
+
+            case qi::ASSIGN:
+                out<<"    mov   eax, "<<addrstr(q.A)<<endl;//value
+                out<<"    mov   "<<addrstr(q.D)<<", eax"<<endl;//assign
+                break;
+
+            case qi::JMP:
+                out<<"    jmp   L"<<q.D.value<<endl;
+                break;
+
+            case qi::JZ:
+                out<<"    mov   eax, "<<addrstr(q.A)<<endl;
+                out<<"    test  eax, eax"<<endl;
+                out<<"    jz    L"<<q.D.value<<endl;
+                break;
+
+            case qi::JNE:
+            case qi::JEQ:
+            case qi::JGT:
+            case qi::JGE:
+            case qi::JLT:
+            case qi::JLE:
+                out<<"    mov   eax, "<<addrstr(q.A)<<endl;
+                out<<"    mov   ecx, "<<addrstr(q.B)<<endl;
+                out<<"    cmp   eax, ecx"<<endl;
+                out<<"    "<<cmpopstr(q.op)<<"   L"<<q.D.value<<endl;
+                break;
+
+            case qi::MUL:
+                out<<"    mov   eax, "<<addrstr(q.A)<<endl;
+                out<<"    imul  eax, "<<addrstr(q.B)<<endl;
+                out<<"    mov   "<<addrstr(q.D)<<", eax"<<endl;
+                break;
+
+            case qi::DIV:
+                out<<"    mov   eax, "<<addrstr(q.A)<<endl;
+                out<<"    cdq"<<endl;
+                out<<"    idiv  "<<addrstr(q.B)<<endl;
+                out<<"    mov   "<<addrstr(q.D)<<", eax"<<endl;
+                break;
+
+            case qi::ADD:
+                out<<"    mov   eax, "<<addrstr(q.A)<<endl;
+                out<<"    add   eax, "<<addrstr(q.B)<<endl;
+                out<<"    mov   "<<addrstr(q.D)<<", eax"<<endl;
+                break;
+
+            case qi::SUB:
+                out<<"    mov   eax, "<<addrstr(q.A)<<endl;
+                out<<"    sub   eax, "<<addrstr(q.B)<<endl;
+                out<<"    mov   "<<addrstr(q.D)<<", eax"<<endl;
+                break;
+
+            case qi::NEG:
+                out<<"    mov   eax, "<<addrstr(q.A)<<endl;
+                out<<"    neg   eax"<<endl;
+                out<<"    mov   "<<addrstr(q.D)<<", eax"<<endl;
+                break;
+
+            default:
+                assert(0);
             }
         }
         out << lblexit << ":" << endl
             << "    mov  esp, ebp" << endl
             << "    pop  ebp" << endl
-            << "    ret" << endl;
+            << "    ret" << endl
+            << endl;
         ++context;
     }
 }
@@ -139,5 +270,9 @@ void genasm()
     genheader();
     gendataseg();
     gencodeseg();
-    *asm_output << out.str();
+
+    out << endl;
+    out << "end init";
+
+    *asm_output << out.str() << endl;
 }
