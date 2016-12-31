8b55fa2d6b1d8d179b76c4941347f9d153b363c2
zzdyyy <zzdyyy@qq.com>
2016-12-31 14:59:04 +0800

    完成了基本块划分

--- actvar.cpp
+++ actvar.cpp
@@ -38,6 +38,33 @@ bool isjc(qi &q)
     }
 }
 
+//return id of bblock; if the label points the exit, return -1
+int lbltobblock(qoperand lbl, vector<qi> &qilist, vector<int> &entrcnt)
+{
+    size_t n=0;
+    size_t len = qilist.size();
+    for(n=0; n<len; ++n)
+    {
+        if(qilist[n].op == qi::LABEL && qilist[n].A.value == lbl.value)
+            break;
+    }
+    assert(n<len);//supposed that would be found
+    do{++n;}while(n<len && qilist[n].op == qi::LABEL);//jump the label
+    if(n<len)
+        return entrcnt[n];
+    else
+        return -1;
+}
+
+//non-repeat push_back
+void push_back_nr(vector<int> &vec, int val)
+{
+    for(int item: vec)
+        if(item == val)
+            return;
+    vec.push_back(val);
+}
+
 void buildbblk()
 {
     //clear bblktbl
@@ -55,39 +82,82 @@ void buildbblk()
         vector<bblk> &bblks = bblktbl[ct];
 
         int cnt = 0;
-        entrcnt[0] = cnt++; bblks.push_back(bblk(0, 0));
         //find all entrances
         for(size_t n=0; n<len; ++n)
         {
+            if(n==0)
+            {
+                while(n<len && qilist[n].op == qi::LABEL)
+                    ++n;
+                if(n<len)
+                    bblks.push_back(bblk(cnt,n));
+                entrcnt[n]=cnt++;
+            }
             //statement with labels or statement after jump instructions
             if(qilist[n].op == qi::LABEL || isjmp(qilist[n]))
             {
                 do{++n;}while(n<len && qilist[n].op == qi::LABEL);
                 if(n<len)
                 {
-                    entrcnt[n--]=cnt++;
-                    bblks.push_back(bblk(cnt-1, n+1));
+                    //cerr<<n<<" ";
+                    bblks.push_back(bblk(cnt, n));
+                    entrcnt[n--]=cnt++;//
                 }
             }
         }
 
-        //build bblks
-        for(size_t n=0; n<len; ++n)
+        //fill exits and flow info
+        for(bblk &b: bblks)
         {
+            //fill exits point
+            size_t n=b.entrance;
+            do{++n;}while(n<len && entrcnt[n]<0);//move n to next entrance
+            do{--n;}while(qilist[n].op == qi::LABEL);//move n to the exit of the bblk
+            b.exit = n;
 
+            //fill flow info, i.e. to and from
+            if(qilist[n].op == qi::RET)
+            {
+                push_back_nr(b.to,-1);//flow to exit
+                continue;
+            }
+            if(qilist[n].op == qi::JMP)
+            {
+                int bbid = lbltobblock(qilist[n].D, qilist, entrcnt);
+                push_back_nr(b.to, bbid);//flow to destination
+                if(bbid>=0)
+                    push_back_nr(bblks[bbid].from, b.id);//flow from b
+                continue;
+            }
+            //maybe flow into next bblock
+            int next = b.id+1;
+            if(size_t(next)>=bblks.size())
+                next = -1;//exit block
+            push_back_nr(b.to, next);
+            if(next>=0)
+                push_back_nr(bblks[next].from, b.id);
+            //maybe flow to other bblock
+            if(isjc(qilist[n]))
+            {
+                int bbid = lbltobblock(qilist[n].D, qilist, entrcnt);
+                push_back_nr(b.to, bbid);
+                if(bbid>=0)
+                    push_back_nr(bblks[bbid].from, b.id);
+            }
         }
+        push_back_nr(bblks[0].from,-1);//begin->block0
 
 #ifdef ACTVAR_DEBUG
-        cerr<<glbtbl[functbl[ct].index].name<<"():"<<endl;
+        cerr<<"BBLOCKs in "<<glbtbl[functbl[ct].index].name<<"():"<<endl;
         for(bblk b : bblks)
         {
             cerr<<setw(4)<<b.id<<setw(4)<<b.entrance<<setw(4)<<b.exit<<" [";
             for(int e: b.from)
                 cerr << setw(4) <<e;
-            cerr<<"] [";
+            cerr<<"  ] [";
             for(int t: b.to)
                 cerr << setw(4) <<t;
-            cerr<<"]"<<endl;
+            cerr<<"  ]"<<endl;
         }
 #endif // ACTVAR_DEBUG
     }
